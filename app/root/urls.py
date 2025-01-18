76fad3572e2410bcd6a492a96cbe31ccf12987c9
Maximilian Eder <Maximilian.eder97@gmail.com>
2025-01-18 23:48:12 +0100

    MVM-12 implemented searchbar for recipes via spoonacular API

--- app/root/urls.py
+++ app/root/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import  views
+
+from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path('recipes/<int:recipe_id>/instructions/', views.recipe_instructions, name='recipe_instructions'),
 ]
