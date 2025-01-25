b8a7071f71ba6306b2dda3f7f7523a51fb3f26d1
renefreitag <r.freitag98@gmail.com>
2025-01-25 01:33:24 +0100

    MVM-16 fixed absolute import; see jira issue comment!

--- app/root/urls/recipe_urls.py
+++ app/root/urls/recipe_urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from ..views.recipe.recipe_view import RecipeSearchView
+from root.views.recipe.recipe_view import RecipeSearchView
 
 urlpatterns = [
     path('search/', RecipeSearchView.as_view(), name='recipe-search'),
