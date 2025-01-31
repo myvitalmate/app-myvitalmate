f121f667aeca3c67c76b93f6bafcac657362b9e5
Maximilian Eder <Maximilian.eder97@gmail.com>
2025-01-31 09:43:42 +0100

    RecipePage including Instructions, Ingredients and Styles

---
+++ app/root/models/models.py
@@ -0,0 +1,6 @@
+# Create your models here.
+#recipes eventuell umbennenn und types angeben
+#ggf. eigenen Parameter angeben, anhand von Calories oder ähnliches.
+#pder ander Werte.
+#Zutaten API call, Instructions in die Website. Gridlayout anlegen?
+#Modelle Erstellen für die unterschiedliche typen. 
--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -3,12 +3,14 @@ import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home/HomePage.tsx";
 import About from "./pages/About/About.tsx";
 import ChatPage from "./pages/Chat/ChatPage.tsx";
+import RecipesPage from "./pages/Recipes/RecipesPage.tsx";
 
 // Define routes in an object format
 const routes = [
     {path: '/', element: <Home/>},
     {path: '/about', element: <About/>},
     {path: '/chat', element: <ChatPage/>},
+    {path: '/recipes', element: <RecipesPage/>}
 ];
 
 const App = () => {
