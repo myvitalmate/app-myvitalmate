32ac24a4b31ac36c138bccddf52576f2bdd60b6c
Rene Freitag <r.freitag98@gmail.com>
2025-02-03 21:06:22 +0100

    added spring boot health check endpoint

--- ui/src/pages/Home/HomePage.tsx
+++ ui/src/pages/Home/HomePage.tsx
@@ -1,6 +1,7 @@
 import {useState} from "react";
 
-const BASE_URL = 'http://127.0.0.1:8000';
+// const BASE_URL = 'http://127.0.0.1:8000';
+const BASE_URL = 'http://localhost:8080';
 
 export const fetchResponse = async (searchQuery: string) => {
     const response = await fetch(`${BASE_URL}/health/check/?query=${encodeURIComponent(searchQuery)}`);
