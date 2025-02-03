dc0aada955855b3b8a94166bc46aee4c7f2775a8
Rene Freitag <r.freitag98@gmail.com>
2025-02-03 20:16:43 +0100

    refactor app path

--- app-spring/src/main/java/com/myvitalmate/app/AppSpringApplication.java
+++ app-spring/src/main/java/com/myvitalmate/app/AppSpringApplication.java
@@ -1,4 +1,4 @@
-package com.myvitalmate.app_spring;
+package com.myvitalmate.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- app-spring/src/test/java/com/myvitalmate/app/AppSpringApplicationTests.java
+++ app-spring/src/test/java/com/myvitalmate/app/AppSpringApplicationTests.java
@@ -1,4 +1,4 @@
-package com.myvitalmate.app_spring;
+package com.myvitalmate.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
