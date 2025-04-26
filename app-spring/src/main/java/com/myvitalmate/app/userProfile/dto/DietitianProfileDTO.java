05dc2e425fe05a34e1935c15be8d222480211c81
Maximilian Eder <Maximilian.eder97@gmail.com>
2025-04-26 13:00:41 +0200

    implemented mapper annotation to reduce boilerplate-code and follow best practices

--- app-spring/src/main/java/com/myvitalmate/app/userProfile/dto/DietitianProfileDTO.java
+++ app-spring/src/main/java/com/myvitalmate/app/userProfile/dto/DietitianProfileDTO.java
@@ -1,7 +1,5 @@
 package com.myvitalmate.app.userProfile.dto;
 
-import com.myvitalmate.app.userProfile.entity.DietitianProfile;
-
 import java.time.LocalDate;
 
 public record DietitianProfileDTO(
@@ -13,23 +11,4 @@ public record DietitianProfileDTO(
         LocalDate birthday,
         String specialty
 ) {
-    public static DietitianProfileDTO fromEntity(DietitianProfile entity) {
-        return new DietitianProfileDTO(
-                entity.getFirstName(),
-                entity.getLastName(),
-                new ContactDTO(
-                        entity.getContact().getEmail(),
-                        entity.getContact().getPhoneNumber()
-                ),
-                new AdresseDTO(
-                        entity.getAdresse().getStreet(),
-                        entity.getAdresse().getCity(),
-                        entity.getAdresse().getCountry(),
-                        entity.getAdresse().getPostalCode()
-                ),
-                entity.getGender(),
-                entity.getBirthday(),
-                entity.getSpecialty()
-        );
-    }
 }
